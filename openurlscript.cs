d96d1cceb29c4fb0fd6a0625c44777f5f6dce091
Kekorot <awdhootkanawade@gmail.com>
2019-09-16 13:24:06 +0530

    Add files via upload

---
+++ openurlscript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class openurlscript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void btnpress()
+    {
+        Application.OpenURL("http://aprgtech.com/web/hack3.html");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
